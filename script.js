f148e752274459ae64ddf25983884cca0162aa76
Dwaimah <eng.adwaimah@gmail.com>
2023-09-05 07:53:44 +0300

    create conflict2 in scroll

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 alert("Abd1");
-alert("Nabil5");
-alert("Abood22");
-alert("Abood3");
-alert("Nabil8");
-alert("Nabil9");
-alert("Nabil10");
-alert("Nabil11");
-alert("Nabil2");
 alert("Nabil3");
 alert("Nabil4");
 alert("scroll1");
